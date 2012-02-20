a6707091d695731007ace4e78582700002a2926a
Matt Bornski <matt@bornski.com>
2012-02-20 10:49:06 -0800

    X10 widget

---
+++ X10.widget/Contents/Javascript/views/sidebar.js
@@ -0,0 +1,63 @@
+/**
+ * @author jstone
+ */
+
+var MainView = new KONtx.Class({
+	ClassName: 'MainView',
+	
+	Extends: KONtx.system.SidebarView,
+
+	createView: function() {
+	  var do = function (house, unit. command) {
+	    var request = new XMLHttpRequest();
+      request.onreadystatechange = getReadyStateHandler();
+      request.open("POST", "http://192.168.10.77/N/2/on");
+      request.timeout = connection_timeout;
+      start_time = animator.milliseconds;
+      request.send();
+	  };
+	  var buttons = [{
+	    label: 'Turn on N2',
+	    events: {
+				onSelect: function(event) {
+				  do('N', '2', 'on');
+				}
+			},
+	  }, {
+		  label: "Turn off N2",
+		  events: {
+  		  onSelect: function(event) {
+  			  do('N', '2', 'off');
+  		  }
+  		},
+  	}];
+	  var vOffset = 0;
+	  for (var index in buttons) {
+	    var options = buttons[index];
+	    if (options['guid'] === undefined) {
+	      options['guid'] = 'button' + index;
+	    }
+	    if (options['styles'] === undefined) {
+	      options['styles'] = {};
+	    }
+	    if (options['styles']['width'] === undefined) {
+	      options['styles']['width'] = Theme.viewSpecs.SIDE_BAR.width;
+	    }
+	    if (options['styles']['height'] === undefined) {
+	      options['styles']['height'] = KONtx.utility.scale(35);
+	    }
+	    if (options['styles']['vOffset'] === undefined) {
+	      options['styles']['vOffset'] = KONtx.utility.scale(vOffset);
+	    }
+	    // TODO determine actual height in case this was overridden.
+	    vOffset += 35;
+	    this.controls[options['guid']] = new KONtx.control.TextButton(options).appendTo(this);
+	  }
+		new KONtx.control.EmptySpace({
+			styles: {
+				vOffset: KONtx.utility.scale(vOffset),
+				height: Theme.viewSpecs.SIDE_BAR.height - KONtx.utility.scale(vOffset),
+			},
+		}).appendTo(this);
+	}
+});
