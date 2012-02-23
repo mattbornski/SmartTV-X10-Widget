ca618015ef84b385cceaf0d8d6b7cee839f1aed8
Matt Bornski <matt@bornski.com>
2012-02-23 07:39:49 -0800

    missing some dependencies apparently

---
+++ X10.widget/Contents/Javascript/views/snippet.js
@@ -0,0 +1,24 @@
+/**
+ * @author jstone
+ */
+
+var StaticSnippetView = new KONtx.Class({
+	ClassName: 'StaticSnippetView',
+	
+	// Pay attention to the fact this one extends from AnchorSnippetView
+	// That is because this snippet always exists in all profiles and 
+	// isn't a "custom" one added per user action
+	Extends: KONtx.system.AnchorSnippetView,
+		
+	createView: function() {
+		this.controls.text = new KONtx.element.Text({
+			label: "Snippet Sample",
+			styles: {
+				fontSize: KONtx.utility.scale(20),
+				vAlign: "center",
+				hAlign: "center",
+				color: "#FFFFFF"
+			},
+		}).appendTo(this);
+	},
+});
